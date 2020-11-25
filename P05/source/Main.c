06d01d0bb162246bd53982a6d7943a60b43520d3
yenlin09 <71508997+yenlin09@users.noreply.github.com>
2020-11-25 22:33:37 +0800

    Add files via upload

--- P05/source/Main.c
+++ P05/source/Main.c
@@ -1,17 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	int n[10] = { 32,27,64,18,95,14,90,70,60,37 };
-	int i;
-
-	printf("%s%13s\n", "Element", "Value");
-
-	for (i = 0; i < 10; i++)
-	{
-		printf("%7d%13d\n", i, n[i]);
-	}
+	int a;
+	int *aPtr;
+	a = 7;
+	aPtr = &a;
+	printf("The address of a is %p"
+		"\nThe value of aPtr is %p", &a, aPtr);
+	printf("\n\nThe value of a is %d"
+		"\nThe value of *aPtr is %d", a, *aPtr);
+	printf("\n\nShowing that * and & are complements of "
+		"each other\n&*aPtr = %p"
+		"\n*&aPtr = %p\n", &*aPtr, *&aPtr);
 	system("pause");
 	return 0;
 }
--- P13/HW/p13/p13/Main.cpp
+++ P13/HW/p13/p13/Main.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cubeByValue(int n);
+int cubeByReference(int &nPtr);
 
 int main(void)
 {
 	int number = 5;
 	printf("The original value of number is %d", number);
 
-	number = cubeByValue(number);
+	number = cubeByReference(number);
 
 	printf("\nThe new value of number is %d\n", number);
 	system("pause");
 	return 0;
 }
 
-int cubeByValue(int n)
+int cubeByReference(int &nPtr)
 {
-	return n * n*n;
+	return nPtr * nPtr * nPtr;
 }
